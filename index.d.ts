423e7320432b84c4ea1d60d0d950e0e74579c92e
nlac <nlacsoft@gmail.com>
2019-09-23 09:21:51 +0200

    corrected definition file

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 import * as React from 'react'
-import Form from "react-jsonschema-form"
+import Form, {FormProps} from 'react-jsonschema-form'
 import {FormRule} from './src/RuledJsonSchemaForm'
 import * as Utils from './src/Utils'
 
-export default class RuledJsonSchemaForm<T> extends Form<T> {}
+export default class RuledJsonSchemaForm<T> extends React.Component<FormProps<T>> {}
 export {
 	FormRule, Utils
 }
